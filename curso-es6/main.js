af83007fb88ce47f3ba5fca30a4102fa426b0f48
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2020-11-27 20:47:47 -0300

    Operação em Array

--- curso-es6/main.js
+++ curso-es6/main.js
@@ -71,3 +71,35 @@ function teste(x) {
 }
 teste(10);
 */
+
+//*******************Operação em Array
+/*
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
+//Map
+const newArr = arr.map(function(item, index) {  //index== posição
+    return item * index;  //pode colocar index para o numero da posição ou outro valor
+});
+console.log(newArr);
+
+//Reduce
+const sum = arr.reduce(function(total, next) {
+    return total + next     //o numero mais o proximo
+});
+console.log(sum);
+
+//filter (Filtra dentro do array)
+const filter = arr.filter(function(item) {
+    return item % 2 === 0;
+});
+
+console.log(filter);
+
+//find  Encontra um valor dentro do array
+const find = arr.find(function(item) {
+    return item === 4;
+});
+console.log(find);
+*/
+
+
